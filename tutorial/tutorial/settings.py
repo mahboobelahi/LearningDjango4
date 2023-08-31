cba3d74197c49da42dd1fdf9a7b07e40f0afeb5e
Mahboob Elahi <46259412+mahboobelahi@users.noreply.github.com>
2023-08-31 19:55:49 +0300

    Models

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'my_app.apps.MyAppConfig',
+    'office.apps.OfficeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
